de0a93492562953c37895ed8b0997972dad15bfd
Kurt Wilson <kurt4wilson@gmail.com>
2019-01-23 15:27:44 -0500

    keep a copy of the networking code

--- src/client/core/ApplicationState.java
+++ src/client/core/ApplicationState.java
@@ -1,12 +1,11 @@
 package client.core;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
+import common.ScannedPaper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import common.ScannedPaper;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class ApplicationState {
