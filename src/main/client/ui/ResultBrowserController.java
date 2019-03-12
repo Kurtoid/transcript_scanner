6da4c27010852623880152b4d9610450a9fd2c4f
Kurt Wilson <kurt4wilson@gmail.com>
2019-03-12 17:31:09 -0400

    testing column stuff; name entry

--- src/main/client/ui/ResultBrowserController.java
+++ src/main/client/ui/ResultBrowserController.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import main.common.FileManager;
 import main.common.ParsedReport;
+import main.common.ReportWriter;
 import main.common.courses.PrereqChecker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -127,5 +128,9 @@ public class ResultBrowserController extends Application implements Initializabl
     public void nextPage(ActionEvent actionEvent) {
         index++;
         updateReport();
+    }
+
+    public void exportReports(ActionEvent actionEvent) {
+        ReportWriter.saveReports(reports);
     }
 }
